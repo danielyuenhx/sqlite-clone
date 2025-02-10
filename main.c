1269dab75f5b0b7029bc7f0fa3006deaf38e5cb1
Daniel Yuen <yuendaniel33@gmail.com>
2025-02-11 00:28:47 +0800

    refactor to process inserts

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "structs.c"
+#include "utils.c"
 
 void read_input(InputBuffer* input_buffer) {
   // read the line from stdin into the input buffer
@@ -16,12 +18,68 @@ void read_input(InputBuffer* input_buffer) {
   to_lower(input_buffer->buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    clean_up_input_buffer(input_buffer);
+    printf("bye");
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNISED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    int args_assigned = sscanf(
+      input_buffer->buffer, "insert %d %s %s", &statement->row_to_insert->id, 
+      statement->row_to_insert->username, statement->row_to_insert->email
+    );
+    if (args_assigned < 3) {
+      return PREPARE_SYNTAX_ERROR;
+    };
+    
+    return PREPARE_SUCCESS;
+  } else if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+  return PREPARE_UNRECOGNISED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+  printf("Executing");
+}
+
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
 
   while (true) {
     print_prompt();
     read_input(input_buffer);
+    
+    // meta commands starting with '.'
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNISED_COMMAND):
+          printf("unrecognised meta command: %s\n", input_buffer->buffer);
+          continue;
+      }
+    }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNISED_STATEMENT):
+        printf("Unrecogniseed keyword at start of '%s'.\n", input_buffer->buffer);
+        continue;
+    }
+
+    execute_statement(&statement); // this is the virtual machine
+    printf("Executed.\n");
   }
   return 0;
 }
--- structs.c
+++ structs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <BaseTsd.h>
 
 typedef struct {
@@ -21,3 +22,34 @@ void clean_up_input_buffer(InputBuffer* input_buffer) {
   free(input_buffer->buffer); // getline() mallocates it if buffer is NULL, so we have to free
   free(input_buffer);
 }
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNISED_COMMAND,
+} MetaCommandResult;
+
+typedef enum { 
+  PREPARE_SUCCESS, 
+  PREPARE_SYNTAX_ERROR,
+  PREPARE_UNRECOGNISED_STATEMENT,
+} PrepareResult;
+
+typedef enum { 
+  STATEMENT_INSERT, 
+  STATEMENT_SELECT 
+} StatementType;
+
+typedef struct {
+  StatementType type;
+  Row row_to_insert;
+  Row row_to_select;
+} Statement;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
